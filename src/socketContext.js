d3475c410ca63055b69053902ba94b09f8e4f205
qraxiss <qraxiss@gmail.com>
2023-08-18 02:02:06 +0300

    url update

--- src/socketContext.js
+++ src/socketContext.js
@@ -1,15 +1,10 @@
 import React, { useState } from 'react'
 import io, { Socket } from 'socket.io-client'
 
-const socket = io("http://localhost:8001");
+const socket = io('https://kale.kapsulteknoloji.org/video-chat-ms')
 
 export const store = React.createContext()
 const [id, setId] = useState()
-export const storeProvider = ({children})=>{
- 
- socket.on('me',(id)=>setId(id))
- 
- 
-
+export const storeProvider = ({ children }) => {
+  socket.on('me', (id) => setId(id))
 }
-
